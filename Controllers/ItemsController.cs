d6e0685ff9a0ea04ae0b82fe2995f7b72cdd0cd8
Linda Andersson <linda.29.andersson@gmail.com>
2023-03-08 13:38:41 +0100

    Setup for controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{   
+    // GET /items
+
+    [ApiController]
+    [Route("[controller]")] // [Route("[items])]
+    public class ItemsController : ControllerBase
+    {
+        
+    }
+}
