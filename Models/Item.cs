8a9b534b67dec265cdef30d48da9eece42e06ca1
Linda Andersson <linda.29.andersson@gmail.com>
2023-03-08 12:55:12 +0100

    Added properties for the class

--- Models/Item.cs
+++ Models/Item.cs
@@ -4,6 +4,9 @@ namespace Catalog.Models
 {
     public record Item
     {
-        public Guid Id { get; init; }
+        public Guid Id { get; init; } // Init insted of private set
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
     }
-}ö
+}
