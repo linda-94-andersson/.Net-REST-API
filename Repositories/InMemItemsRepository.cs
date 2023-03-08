41b12190205f3e06431b7bb3e87aa31109485675
Linda Andersson <linda.29.andersson@gmail.com>
2023-03-08 13:35:17 +0100

    Added getters for the items

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,6 +1,7 @@
-using System.ComponentModel;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq; 
 using Catalog.Models;
 
 namespace Catalog.Repositories
@@ -13,5 +14,15 @@ namespace Catalog.Repositories
             new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow},
             new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow}
         }; 
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items; 
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault(); 
+        }
     }
 }
