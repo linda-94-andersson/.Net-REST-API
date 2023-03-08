ffc53121fd891d8467c0323a73fa1328606a0dc0
Linda Andersson <linda.29.andersson@gmail.com>
2023-03-08 15:03:51 +0100

    Setup for Extensions

---
+++ Extensions.cs
@@ -0,0 +1,13 @@
+using Catalog.Dtos;
+using Catalog.Models;
+
+namespace Catalog
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+
+        }
+    }
+}
