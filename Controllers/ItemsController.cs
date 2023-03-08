b111c121186cd815c1fccd6c1db6de81a838f513
Linda Andersson <linda.29.andersson@gmail.com>
2023-03-08 13:46:32 +0100

    Created routing for get/items

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,13 +1,27 @@
+using System.Collections.Generic;
+using Catalog.Models;
+using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.Controllers
 {   
-    // GET /items
-
     [ApiController]
     [Route("[controller]")] // [Route("items")]
     public class ItemsController : ControllerBase
     {
-        
+        private readonly InMemItemsRepository repository; 
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository(); 
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems(); 
+            return items; 
+        }
     }
 }
